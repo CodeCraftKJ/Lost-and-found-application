b122e0ccdadf39f5f95e13d5ec101e82101329b1
CodeCraftKJ <krzysztofjofficial@gmail.com>
2024-05-23 23:07:33 +0200

    Setup Changes

--- ItemManager/Setup.cs
+++ ItemManager/Setup.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ItemManager.Data;
 using ItemManager.Repositories.LostItemRepository;
 using ItemManager.Repositories.FoundItemRepository;
+using ItemManager.Services;
 using ItemManager.Configuration;
 using ItemManager.Repositories;
-using ItemManager.Services;
 
 namespace ItemManager
 {
@@ -15,12 +14,7 @@ namespace ItemManager
         public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
             // Load AppSettings
-            var appSettings = configuration.GetSection("AppSettings").Get<AppSettings>();
-
-            // Configure the database context
-            services.AddDbContext<DBContext>(options =>
-                options.UseSqlServer(appSettings.DefaultConnection));
-
+            var appSettings = configuration.GetSection("AppSettings").GetConnectionString();
             // Register repositories
             services.AddScoped<ILostItemRepository, LostItemRepository>();
             services.AddScoped<IFoundItemRepository, FoundItemRepository>();
@@ -29,8 +23,6 @@ namespace ItemManager
             services.AddScoped<ILostItemService, LostItemService>();
             services.AddScoped<IFoundItemService, FoundItemService>();
 
-            // Add other services and configurations as needed
-            services.AddControllers();
         }
     }
 }
