75626f9ecd9917c7abefd80f695a92fc52cc67cd
CodeCraftKJ <krzysztofjofficial@gmail.com>
2024-05-24 21:16:08 +0200

    SetUp Final Bug Fix

--- ItemManager/DbContextr.cs
+++ ItemManager/DbContextr.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Reflection.Emit;
 using ItemManager.Models.FoundItems;
 using ItemManager.Models.LostItems;
 
@@ -8,16 +6,24 @@ namespace ItemManager.Data
 {
     public class DBContext : DbContext
     {
-        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        private readonly string _connectionString;
+
+        public DBContext(string connectionString)
         {
+            _connectionString = connectionString;
         }
 
         public DbSet<LostItem> LostItems { get; set; }
         public DbSet<FoundItem> FoundItems { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+           
         }
     }
 }
--- ItemManager/Setup.cs
+++ ItemManager/Setup.cs
@@ -3,8 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using ItemManager.Data;
 using ItemManager.Repositories.LostItemRepository;
 using ItemManager.Repositories.FoundItemRepository;
-using ItemManager.Services;
 using ItemManager.Configuration;
+using ItemManager.Services;
 using ItemManager.Repositories;
 
 namespace ItemManager
@@ -13,16 +13,15 @@ namespace ItemManager
     {
         public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
-            // Load AppSettings
-            var appSettings = configuration.GetSection("AppSettings").GetConnectionString();
-            // Register repositories
+            var defaultConnection = configuration.GetSection("AppSettings:DefaultConnection").Value;
+
+            services.AddSingleton(new DBContext(defaultConnection));
+
             services.AddScoped<ILostItemRepository, LostItemRepository>();
             services.AddScoped<IFoundItemRepository, FoundItemRepository>();
 
-            // Register services
             services.AddScoped<ILostItemService, LostItemService>();
             services.AddScoped<IFoundItemService, FoundItemService>();
-
         }
     }
 }
